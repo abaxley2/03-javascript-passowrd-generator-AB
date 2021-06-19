87478a538a88836213803397cef7ed9b2ff66da1
Austin Baxley <ausbaxley@gmail.com>
2021-06-18 22:21:17 -0400

    updated readme and added generator function

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # 03-javascript-passowrd-generator-AB
+
+## Description
+The assignment was to create a functional password generator that prompts the user for input criteria and 
+displays a randomized password. I first created functions that generate random characters. I then created window promps, alerts, 
+and confirms to gather user date. I then assigned each of these to an array and created a for loop to generate a password.
+
+![picture of work](https://abaxley2.github.io/03-javascript-passowrd-generator-AB/password-gen.png)
+## Dependencies
+This application will work on a desktop alone.
+
+## Authors
+
+Austin Baxley
+AusBaxley@gmail.com
+
+## Link to project
+https://abaxley2.github.io/03-javascript-passowrd-generator-AB/
--- script.js
+++ script.js
@@ -1,81 +1,68 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lenthEl = document.getElementById("length");
-var upperEl = document.getElementById("upper");
-var lowerEl = document.getElementById("lower");
-var symbolEl = document.getElementById("symbol");
+//creating string for random values
 
-
-//checking user inputs in console log
-// console.log(userLength);
-// console.log(userUpper);
-// console.log(userLower);
-// console.log(userSymbols);
-
-const randomPassword = {
-  lower: randomlowerCase,
-  upper: randomUpperCase,
-  number: randomPassNumber,
-  symbol: randomPassSym,
+const randomChars = {
+  lower: randomlowerCase(), 
+  upper: randomUpperCase(), 
+  number: randomPassNumber(), 
+  symbol: randomPassSym()
 };
 
 
 // Write password to the #password input
 function writePassword() {
-  // var length =
+  const typesCount = userSelectionsArray.length;
+  let generatedPassword = "";
+  if(typesCount === 0) {
+    return '';
+  }
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  for (let i = 0; i < userLength ; i += typesCount) {
+    userSelectionsArray.forEach(type => {
+      const pwordNames = Object.keys(type)[0];
+      generatedPassword += randomChars[pwordNames]();
+    });
+  }
+ var password = generatedPassword
+ var passwordText = document.querySelector("#password");
+ passwordText.value = password;
 }
 
 //Functions that generate random characters
 function randomlowerCase() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-
 function randomUpperCase() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-
 function randomPassNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-
 function randomPassSym() {
   const symbols = "!@#$%^&*";
   return symbols[Math.floor(Math.random()*symbols.length)];
 }
 
 
-//checking random values in console
-// console.log(randomPassSym());
-// console.log(randomlowerCase());
-// console.log(randomUpperCase());
-// console.log(randomPassNumber());
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 window.alert("Welcome to my password generator!");
-var userLength = window.prompt("Enter Length of password:");
+var userLengthString = window.prompt("Enter Length of password:");
 var userUpper = window.confirm("Would you like uppercase letters? Cancel if no.");
 var userLower = window.confirm("Would you like lowercase letters? Cancel if no.");
 var userNumbers = window.confirm("Would you like numbers? Cancel if no.")
 var userSymbols = window.confirm("Would you like special characters? Cancel if no.");
 
+var userLength = parseInt(userLengthString);
+
 //creating array for user input
 var userSelectionsArray = [{userUpper}, {userLower}, {userNumbers}, {userSymbols}].filter(item => Object.values(item)[0]);
-console.log(userSelectionsArray);
-
-// generating password
-function generatePassword() {
-
-  for (let i = 0; i < userLength; i += userSelectionsArray)
 
 
-}
+
+
+
+
